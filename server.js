a5fb699b3c28f3f7ef65c00cbce7868cf6a0d3f4
Sergeevich <mrssn84@mail.ru>
2022-07-15 14:49:21 +0300

    fd

--- server.js
+++ server.js
@@ -3,19 +3,9 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
 	console.log (req.url)
-	const body = req.url //=== '/style.css'
-	//?
-	//:	
-	if (req.url === '/style.css') {
-		fs.readFileSync('./public/style.css')
-	} else {
-		fs.readFileSync('./public/index.html');
-	}
-    return
-	//} else {
-
-
-	//} 
+	const body = req.url === '/style.css'
+	? fs.readFileSync('./public/style.css')
+	: fs.readFileSync('./public/index.html');	
 	res.end(body)
 })
 server.listen(3000)
